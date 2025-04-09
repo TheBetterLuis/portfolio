847ce7eb3f7de7be6d0bfcbc6d0cd04867edead0
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2025-04-09 00:44:16 -0400

    skdjfksd

--- script.js
+++ script.js
@@ -10,6 +10,26 @@ function highlightButton(index) {
   footerLinks.forEach((element) => {
     element.classList.remove("selected");
   });
-
   footerLinks[index].classList.add("selected");
+  changeCommand(index);
+}
+
+function changeCommand(number) {
+  let message = "./";
+  switch (number) {
+    case 0:
+      message += "about-me";
+      break;
+    case 1:
+      message += "skills";
+      break;
+    case 2:
+      message += "contact";
+      break;
+    default:
+      message += "not-found";
+      break;
+  }
+  const command = document.getElementById("command");
+  command.innerHTML = message;
 }
