8d66c3fac4f7e9fe430afa00bf4d82ef2e967fe8
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2025-04-28 03:03:39 -0400

    hash changes are working

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,11 +1,51 @@
+const pages = document.querySelectorAll(".page");
 const sectionButtons = document.querySelectorAll(".section-button");
-sectionButtons[0].classList.add("selected");
 let currentPage = 0;
+
+function handleHashChange() {
+  if (location.hash) {
+    let theName = location.hash.slice(1);
+    pages.forEach((element) => {
+      if (element.classList.contains("visible")) {
+        element.classList.remove("visible");
+      }
+    });
+    document.querySelector(`.${theName}`).classList.add("visible");
+
+    sectionButtons.forEach((element, index) => {
+      element.classList.remove("selected");
+      if (element.innerText.toLowerCase() === theName) {
+        element.classList.add("selected");
+        currentPage = index;
+      }
+    });
+    updateHash(currentPage);
+  } else {
+    document.querySelector(`.about-me`).classList.add("visible");
+    history.replaceState({}, "", `/portfolio/#about-me`);
+    sectionButtons[0].classList.add("selected");
+  }
+}
+
+handleHashChange();
+
+window.addEventListener("hashchange", handleHashChange);
+
+function updateHash(index) {
+  history.pushState(
+    {},
+    "",
+    `/portfolio/#${sectionButtons[index].innerText.toLowerCase()}`
+  );
+  document.title = `${sectionButtons[index].innerText.toLowerCase()}`;
+}
 
 sectionButtons.forEach((element, index) => {
   sectionButtons[index].addEventListener("click", () => {
     currentPage = index;
     highlightButton(index);
+    updateHash(index);
+    handleHashChange(index);
   });
 });
 
@@ -27,6 +67,8 @@ document.addEventListener("keydown", (event) => {
       }
       highlightButton(currentPage);
       event.preventDefault();
+      changeTitle(currentPage);
+      updateHash(currentPage);
       break;
     case "ArrowUp":
     case "k":
@@ -37,6 +79,21 @@ document.addEventListener("keydown", (event) => {
       }
       highlightButton(currentPage);
       event.preventDefault();
+      changeTitle(currentPage);
+      updateHash(currentPage);
       break;
   }
 });
+
+function changeTitle(index) {
+  const title = document.getElementById("current-page");
+  title.innerText = sectionButtons[index].innerText;
+
+  pages.forEach((element) => {
+    if (element.classList.contains("visible")) {
+      element.classList.remove("visible");
+    }
+  });
+
+  pages[index].classList.add("visible");
+}
